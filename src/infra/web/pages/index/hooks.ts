0075353e477c5c1433c287848a2ad491cbafd879
Technote <technote.space@gmail.com>
2021-12-09 03:13:27 +0900

    chore: test apis

--- src/infra/web/pages/index/hooks.ts
+++ src/infra/web/pages/index/hooks.ts
@@ -2,6 +2,8 @@ import type { Props } from '$/web/pages';
 import type { IAuth, UserResult } from '$/web/shared/auth';
 import type { client } from '@/web/shared/api';
 import useAspidaSWR from '@aspida/swr';
+import dayjs from 'dayjs';
+import { useCallback } from 'react';
 
 const getAuthorization = (user: UserResult): string => {
   console.log(user);
@@ -16,19 +18,51 @@ const getAuthorization = (user: UserResult): string => {
 export const useHooks = (props: Props, auth: IAuth, api: typeof client) => {
   const user = auth.useUser();
   const handleLogout = auth.useLogout();
-
-  const { data: tasks } = useAspidaSWR(
+  const { data: tasks, mutate } = useAspidaSWR(
     api.tasks,
     {
       headers: { authorization: getAuthorization(user) },
       enabled: user.isLoggedIn,
     },
   );
+  const handleDelete = useCallback((id: string) => {
+    console.log(id);
+    api.tasks._taskId(id).delete({ headers: { authorization: getAuthorization(user) } }).then(() => mutate());
+  }, [user, api.tasks, mutate]);
+  const handleAdd = useCallback(() => {
+    api.tasks.post({
+      body: {
+        タスク名: '追加テスト',
+        メモ: '追加テストのメモ',
+        ステータス: '登録',
+        期日: dayjs().add(5, 'days').toISOString(),
+        作業見積: 3,
+        作業見積単位: '日',
+        タグ: ['追加テストのタグ１', '追加テストのタグ２'],
+      }, headers: { authorization: getAuthorization(user) },
+    }).then(() => mutate());
+  }, [user, api.tasks, mutate]);
+  const handleUpdate = useCallback((id: string) => {
+    api.tasks._taskId(id).put({
+      body: {
+        タスク名: '更新テスト',
+        メモ: null,
+        ステータス: '実行中',
+        期日: null,
+        作業見積: null,
+        作業見積単位: '時間',
+        タグ: ['更新テストのタグ１'],
+      }, headers: { authorization: getAuthorization(user) },
+    }).then(() => mutate());
+  }, [user, api.tasks, mutate]);
 
   return {
     user,
     handleLogout,
     tasks,
+    handleAdd,
+    handleUpdate,
+    handleDelete,
   };
 };
 export type HooksParams = ReturnType<typeof useHooks>;
--- src/infra/web/pages/index/view.tsx
+++ src/infra/web/pages/index/view.tsx
@@ -1,8 +1,40 @@
 import type { HooksParams } from './hooks';
+import type { TaskDto } from '^/usecase/task/taskDto';
 import type { VFC } from 'react';
-import { memo } from 'react';
+import { memo, useCallback } from 'react';
 
-const View: VFC<HooksParams> = ({ user, handleLogout, tasks }) => {
+const Task: VFC<{ task: TaskDto; handleUpdate: (id: string) => void; handleDelete: (id: string) => void }> = ({
+  task,
+  handleUpdate,
+  handleDelete,
+}) => {
+  const _handleUpdate = useCallback(() => {
+    handleUpdate(task.id);
+  }, [task.id]);
+  const _handleDelete = useCallback(() => {
+    handleDelete(task.id);
+  }, [task.id]);
+  return <div key={task.id} style={{ border: 'solid 1px #ccc', margin: '10px', padding: '10px' }}>
+    <div>タスク名: {task.タスク名}</div>
+    <div>メモ: {task.メモ}</div>
+    <div>ステータス: {task.ステータス}</div>
+    <div>期日: {task.期日}</div>
+    <div>作業見積: {task.作業見積}</div>
+    <div>作業見積単位: {task.作業見積単位}</div>
+    <div>タグ: {task.タグ.join(', ')}</div>
+    <button role="update" onClick={_handleUpdate}>Update</button>
+    <button role="delete" onClick={_handleDelete}>Delete</button>
+  </div>;
+};
+
+const View: VFC<HooksParams> = ({
+  user,
+  tasks,
+  handleLogout,
+  handleAdd,
+  handleUpdate,
+  handleDelete,
+}) => {
   if (!user.isLoggedIn) {
     return <div>Loading...</div>;
   }
@@ -10,15 +42,13 @@ const View: VFC<HooksParams> = ({ user, handleLogout, tasks }) => {
   return <div>
     <div>Hello World!</div>
     <button role="logout" onClick={handleLogout}>Logout!</button>
-    {tasks?.map(task => <div key={task.id}>
-      <div>タスク名: {task.タスク名}</div>
-      <div>メモ: {task.メモ}</div>
-      <div>ステータス: {task.ステータス}</div>
-      <div>期日: {task.期日}</div>
-      <div>作業見積: {task.作業見積}</div>
-      <div>作業見積単位: {task.作業見積単位}</div>
-      <div>タグ: {task.タグ.join(', ')}</div>
-    </div>)}
+    <button role="add" onClick={handleAdd}>Add!!!</button>
+    {tasks?.map(task => <Task
+      key={task.id}
+      task={task}
+      handleUpdate={handleUpdate}
+      handleDelete={handleDelete}
+    />)}
   </div>;
 };
 
