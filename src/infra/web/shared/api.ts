cd4dfccd915f17263034e3c343272a56c07f592d
Technote <technote.space@gmail.com>
2021-12-12 04:07:06 +0900

    chore: consider auth error

--- src/infra/web/shared/api.ts
+++ src/infra/web/shared/api.ts
@@ -1,6 +1,7 @@
 import type { IApi, ApiType, ApiSelector, Caller, ResponseData, SWRResponse } from '$/web/shared/api';
 import type { IAuthContext } from '$/web/shared/auth';
 import type { ApiInstance } from '^/pages/api/$api';
+import type { AxiosError } from 'axios';
 import useAspidaSWR from '@aspida/swr';
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
@@ -14,13 +15,22 @@ export class Api implements IApi {
   ) {
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private static isAxiosError(error?: any): error is AxiosError {
+    return !!error?.isAxiosError;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private static isAuthError(error?: any): boolean {
+    return Api.isAxiosError(error) && error.response?.status === 401;
+  }
+
   public useSWR<T extends ApiType>(selector: ApiSelector<T>): SWRResponse<ResponseData<T['$get']>> {
     const dispatch = useDispatch();
     const [api, ...option] = selector(this.client);
     const result = useAspidaSWR(api, ...option);
 
-    if (result.error) {
-      // TODO: 認証エラーの場合にログアウト状態にする
+    if (Api.isAuthError(result.error)) {
       this.authContext.setUser(dispatch, { isLoggedIn: false });
     }
 
@@ -34,8 +44,10 @@ export class Api implements IApi {
       try {
         return await generatePromise(this.client);
       } catch (e) {
-        // TODO: 認証エラーの場合にログアウト状態にする
-        this.authContext.setUser(dispatch, { isLoggedIn: false });
+        if (Api.isAuthError(e)) {
+          this.authContext.setUser(dispatch, { isLoggedIn: false });
+        }
+
         throw e;
       }
     }, [dispatch]);
