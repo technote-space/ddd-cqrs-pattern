16a911352c9c8859d89cc86475edf64448565fa5
Technote <technote.space@gmail.com>
2022-01-15 21:21:57 +0900

    fix: aspida url

--- aspida.config.js
+++ aspida.config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   input: 'src/pages/api',
-  baseURL: `${(process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : undefined) || process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'}/api`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_ENDOPOINT ?? 'http://localhost:3000'}/api`,
   trailingSlash: true,
 };
